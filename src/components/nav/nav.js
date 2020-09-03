8c050bc050e4e94f518c3e7a0336f86198c3f0d1
Ryan Milne <ryanmilne101@gmail.com>
2020-09-03 16:12:29 -0600

    media

--- src/components/nav/nav.js
+++ src/components/nav/nav.js
@@ -3,8 +3,8 @@ import { Link } from "react-router-dom";
 import { connect } from "react-redux";
 import { logoutPlayer, getPlayer } from "../../ducks/reducer";
 import axios from "axios";
-import "./Nav.css"
-import MenuIcon from '../MenuIcon'
+import "./Nav.css";
+import MenuIcon from "../MenuIcon";
 
 const Nav = (props) => {
   const [toggle, setToggle] = useState(false);
@@ -24,27 +24,46 @@ const Nav = (props) => {
   };
 
   return (
-    <div className = 'nav-main'>
+    <div className="nav-main">
       <img
-      className="logo-img"
+        className="logo-img"
         alt="Logo"
         src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSilC7N-AsDXEGBNiKpMdTxUehiR-_kF9tvLg&usqp=CAU"
       />
-      <h1 className='head-text'>Alley-Hoop</h1>
-      <div className='ham-menu' onClick={() => setToggle(!toggle)}><MenuIcon/></div>
+      <h1 className="head-text">Alley-Hoop</h1>
+      <div className="ham-menu" onClick={() => setToggle(!toggle)}>
+        <MenuIcon />
+      </div>
       {toggle ? (
         <nav className="mobile-toggle">
           <span onClick={() => setToggle(!toggle)}>
-            <Link className='mo' to="/games">Home</Link>
+            <Link className="mo" to="/games">
+              Home
+            </Link>
           </span>
           <span onClick={() => setToggle(!toggle)}>
-            <Link className='mo' to="/profile">Profile</Link>
+            <Link className="mo" to="/profile">
+              Profile
+            </Link>
           </span>
           <span onClick={logout}>
-            <Link className='mo' to="/">Logout</Link>
+            <Link className="mo" to="/">
+              Logout
+            </Link>
           </span>
         </nav>
       ) : null}
+      <div className="desktop-menu">
+        <Link className="d-mo" to="/games">
+          Home
+        </Link>
+        <Link className="d-mo" to="/profile">
+          Profile
+        </Link>
+        <Link className="d-mo" to="/">
+          Logout
+        </Link>
+      </div>
     </div>
   );
 };
