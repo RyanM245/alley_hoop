ef2eb38c85fc21b78c628a5387472d564ee1e8d4
Ryan Milne <ryanmilne101@gmail.com>
2020-09-10 09:52:23 -0600

    Before Hosting

--- src/components/game/Game.js
+++ src/components/game/Game.js
@@ -5,8 +5,10 @@ const Game = (props) => {
   console.log(props.data)
   return (
     <div className="game-holder">
+      <div className="game-id-maker">
       <h1 className='game-list text'> {props.data.username}</h1>
-       <img src={props.data.pic} className='pro-img'/>
+       <img src={props.data.pic} className='game-img'/>
+       </div>
       <h1 className='game-list text'>Address: {props.data.address}</h1>
       <h1 className='game-list text'>City: {props.data.city}</h1>
       <h1 className='game-list text'>State: {props.data.state_abbrev}</h1>
